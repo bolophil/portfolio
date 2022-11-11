47350a3d0b81f4d3236df41299498cf1afcf9ddb
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro-9.local>
2022-11-10 22:34:10 -0500

    add new project

--- assets/js/main.js
+++ assets/js/main.js
@@ -341,6 +341,7 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img1 = document.getElementById("myImg1");
 var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
 var modalImg = document.getElementById("img01");
 img1.onclick = function(){
   modal.style.display = "block";
@@ -350,6 +351,11 @@ img2.onclick = function(){
 	modal.style.display = "block";
 	modalImg.src = this.src;
   }
+img3.onclick = function(){
+	modal.style.display = "block";
+	modalImg.src = this.src;
+}
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
